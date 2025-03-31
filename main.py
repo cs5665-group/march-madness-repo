97f0ee1cc478ff68929c5e6eeb707a85eaceacd5
Hunter Peart <hman1148@outlook.com>
2025-03-31 15:10:19 -0600

    Added addtional epochs to model

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from models.section1.ingestor import Ingestor
 from models.section1.model import StarterModel
 
+<<<<<<< Updated upstream
+=======
+if __name__ == "__main__":
+    
+    # File paths
+    training_data_path = './datasets/MNCAATourneyDetailedResults.csv'
+    submission_template_path = './datasets/SampleSubmissionStage1.csv'
+    model_path = './matchup_prediction_model.pth'
+        
+    X_train, X_test, y_train, y_test, team_ids = load_and_preprocess_data(training_data_path)
+    
+    # Train the model
+    print("Training the model...")
+    train_model(training_data_path, num_epochs=20, batch_size=64, learning_rate=0.001)
+    
+    # Generate the submission file
+    print("Generating the submission file...")
+    generate_submission(team_ids, model_path)
+    print("Submission file generated successfully.")
+    
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
     ingestor:Ingestor = Ingestor()
